e280338924ee1dc9c44d05a68695c24b4d52e944
Pavlo Mohyla <pm882205@gmail.com>
2025-07-01 18:01:56 +0300

    Trigger CI/CD test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Docker!');
+  res.end('Hello from Docker! Hello from CI/CD world!');
 });
 
 server.listen(3000, () => {
